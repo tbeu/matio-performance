76421cd36caa3f0acc416140ac6a9978405ac8c7
tbeu <tc@tbeu.de>
2017-06-19 11:15:40 +0200

    Update matPerf
    * Add command line flags -i (read info only or all) and -m (use MATLAB API or Matio)
    * Use high perfomance counter

--- src/matPerf.cpp
+++ src/matPerf.cpp
@@ -2,22 +2,21 @@
 #include <windows.h>
 #include <ctime>
 #include <string>
+#include "mat.h"
+#include "matio.h"
 
 using namespace std;
 
-#define MATIO
+typedef void (*ReadMatFile_Proc)(const char* file, bool bInfoOnly);
 
-#if !defined(MATIO)
-#include "mat.h"
-static void ReadMatFile(const char* file)
+static void ReadMatFileMATLAB(const char* file, bool bInfoOnly)
 {
 	MATFile *pmat;
 	mxArray *pa;
 	const char *name;
 	int varCnt = 0;
 
-	
-	cout << "Try to read all variables in: " << file << endl;
+	cout << "Try to read all variables " << (bInfoOnly ? "info in: " : "in: ") << file << endl;
 
 	pmat = matOpen(file, "r");
 	if (pmat == NULL) 
@@ -26,7 +25,7 @@ static void ReadMatFile(const char* file)
 		return;
 	}
 
-	while ((pa = matGetNextVariable(pmat, &name)) != NULL) 
+	while ((pa = bInfoOnly ? matGetNextVariableInfo(pmat, &name) : matGetNextVariable(pmat, &name)) != NULL) 
 	{
 		varCnt++;
 		mxDestroyArray(pa);
@@ -41,15 +40,14 @@ static void ReadMatFile(const char* file)
 	cout << varCnt << " variables found and read in file..." << endl;
 	return;
 }
-#else
-#include "matio.h"
-static void ReadMatFile(const char* file)
+
+static void ReadMatFileMatio(const char* file, bool bInfoOnly)
 {
 	mat_t *pmat;
 	matvar_t *pa;
 	int varCnt = 0;
 	
-	cout << "Try to read all variables in: " << file << endl;
+	cout << "Try to read all variables " << (bInfoOnly ? "info in: " : "in: ") << file << endl;
 
 	pmat = Mat_Open(file, MAT_ACC_RDONLY);
 	if (pmat == NULL) 
@@ -58,7 +56,7 @@ static void ReadMatFile(const char* file)
 		return;
 	}
 
-	while ((pa = Mat_VarReadNext(pmat)) != NULL)
+	while ((pa = bInfoOnly ? Mat_VarReadNextInfo(pmat) : Mat_VarReadNext(pmat)) != NULL)
 	{
 		varCnt++;
 		Mat_VarFree(pa);
@@ -72,9 +70,8 @@ static void ReadMatFile(const char* file)
 	cout << varCnt << " variables found and read in file..." << endl;
 	return;
 }
-#endif
 
-int main()
+int main(int argc, char *argv[])
 {
 	string pathToFiles = "..\\data\\";
 	string fileExtension = "*.mat";
@@ -82,21 +79,43 @@ int main()
 
 	memset(&search_data, 0, sizeof(WIN32_FIND_DATA));
 
+	LARGE_INTEGER frequency;
+	if (0 == QueryPerformanceFrequency(&frequency))
+		return EXIT_FAILURE;
+
+	// -i flag: Read variable info only (and skip data)
+	bool bInfoOnly;
+	if (argc > 1 && 0 == strcmp(argv[1], "-i"))
+		bInfoOnly = true;
+	else if (argc > 2 && 0 == strcmp(argv[2], "-i"))
+		bInfoOnly = true;
+	else
+		bInfoOnly = false;
+
+	// -m flag: Use MATLAB API
+	ReadMatFile_Proc ReadMatFile;
+	if (argc > 1 && 0 == strcmp(argv[1], "-m"))
+		ReadMatFile = ReadMatFileMATLAB;
+	else if (argc > 2 && 0 == strcmp(argv[2], "-m"))
+		ReadMatFile = ReadMatFileMATLAB;
+	else
+		ReadMatFile = ReadMatFileMatio;
+
 	HANDLE handle = FindFirstFile((pathToFiles+fileExtension).c_str(), &search_data);
-	cout << "Tick!" << endl;
-	
-	clock_t startTime = clock();
-	while (handle != INVALID_HANDLE_VALUE)
+	cout << "API: " << (ReadMatFile == ReadMatFileMatio ? "Matio" : "MATLAB") << endl;
+
+	LARGE_INTEGER startTime, endTime;
+	QueryPerformanceCounter(&startTime);
+	while (INVALID_HANDLE_VALUE != handle)
 	{
-		ReadMatFile((pathToFiles+search_data.cFileName).c_str());
+		ReadMatFile((pathToFiles+search_data.cFileName).c_str(), bInfoOnly);
 		if (FindNextFile(handle, &search_data) == FALSE)
 			break;
 	}
-	clock_t endTime = clock();
-	cout << "Tock!" << endl;
+	QueryPerformanceCounter(&endTime);
 
 	FindClose(handle);
-	cout << "Done in: " << ((float)(endTime - startTime) / CLOCKS_PER_SEC) << endl;
-	system("pause");
-	return 0;
+	cout << (bInfoOnly ? "Reading info" : "Reading") << " done in: " << fixed << double(endTime.QuadPart - startTime.QuadPart)/(double)frequency.QuadPart << endl;
+	//system("pause");
+	return EXIT_SUCCESS;
 }
